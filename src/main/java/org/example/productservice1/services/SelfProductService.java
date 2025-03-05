157785367986e4af04fb78e8526245ce9877437c
Karan Vyas <kvyas351@gmail.com>
2025-03-05 19:21:57 +0530

    applied sort in pagination

--- src/main/java/org/example/productservice1/services/SelfProductService.java
+++ src/main/java/org/example/productservice1/services/SelfProductService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -80,7 +81,9 @@ public class SelfProductService implements ProductService {
 
     @Override
     public Page<Product> findAllProductsPaginated(int pageNo, int size) {
-        Page<Product> paginatedProducts = productRepository.findAll(PageRequest.of(pageNo, size));
+        Page<Product> paginatedProducts = productRepository.findAll(PageRequest.of(pageNo,
+                size,
+                Sort.by("id").descending()));
         return paginatedProducts;
     }
 }
